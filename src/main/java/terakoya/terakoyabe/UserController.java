0c3152636eee3e81459a721dff4349676eecf40d
DWaveletT <dingwentao2004@126.com>
2024-04-18 18:19:39 +0800

    调整了 login 的写法

--- src/main/java/terakoya/terakoyabe/UserController.java
+++ src/main/java/terakoya/terakoyabe/UserController.java
@@ -1,12 +1,10 @@
 package terakoya.terakoyabe;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -19,55 +17,34 @@ public class UserController {
                 .body(e.getMessage());
     }
 
-    // 登录成功
-    ResponseEntity<?> loginSuccess(){
-        String token = "123";
-
-        return ResponseEntity.ok()
-                .body(token);
-
-//        return ResponseEntity.ok().build();
-    }
-    // 登录失败，返回401，并将errorMessage设置为
-    ResponseEntity<?> loginFail(String errorMessage){
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(errorMessage);
+    @AllArgsConstructor
+    @Data
+    public static class ErrorResponse {
+        String message;
     }
 
+    @Data
+    public static class LoginRequest {
+        String username;
+        String password;
+    }
 
-    /*
-    function handleClick1() {
-    let username = "test";
-    let password = "123";
-    axios.post(
-        "http://localhost:9999/api/user/login",
-        qs.stringify({
-            username: username,
-            password: password
-        })
-    ).then(res => {
-        console.log(res.data);
-        sessionStorage.setItem("username", username);
-        sessionStorage.setItem("password", password);
-    }).catch(err => {
-        console.log(err);
-    });
-}
+    @AllArgsConstructor
+    @Data
+    public static class LoginResponse {
+        String token;
+    }
 
-     */
     @PostMapping("/login")
-    public ResponseEntity<?> login(String username, String password){
+    public ResponseEntity<?> login(@RequestBody LoginRequest data){
         try {
-            System.out.print(username);
-            if (username.equals("test")){
-                return loginSuccess();
+            if (data.username.equals("test")){
+                return ResponseEntity.ok(new LoginResponse("yjsp114514"));
             } else {
-                return loginFail("用户名或密码错误");
+                return ResponseEntity.status(401).body(new ErrorResponse("用户名或密码错误"));
             }
         } catch (Exception e) {
             return serverError(e);
         }
-    } 
-
-
+    }
 }

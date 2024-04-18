b0fb0f1b4e329840478cbf9448b3d1d340516dbf
OhanaTyan <2449581634@qq.com>
2024-04-18 14:44:25 +0800

    新建项目

---
+++ src/main/java/terakoya/terakoyabe/TerakoyaBeApplication.java
@@ -0,0 +1,13 @@
+package terakoya.terakoyabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerakoyaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerakoyaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/terakoya/terakoyabe/TerakoyaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package terakoya.terakoyabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerakoyaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

89fb19e113b77f38093140f6c426daa8f7a332e8
OhanaTyan <2449581634@qq.com>
2024-04-29 19:51:00 +0800

    修改了log的实现

--- src/main/java/terakoya/terakoyabe/util/ServerError.java
+++ src/main/java/terakoya/terakoyabe/util/ServerError.java
@@ -3,19 +3,13 @@ package terakoya.terakoyabe.util;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.*;
-
-import static terakoya.terakoyabe.MyUtil.getCurrentTime;
-import static terakoya.terakoyabe.util.Log.getPrintWriter;
-
 @Getter
 @Setter
 public class ServerError {
     String message;
 
     public ServerError(Exception e){
-        // TODO: 增加适用于linux的log
-        e.printStackTrace();
+        Log.error(e);
         message = e.toString();
     }
 }
